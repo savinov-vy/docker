9df16ee18cfb0537142115bd186879a315a55c49
savinov <savinov_vu@mail.ru>
2022-02-02 23:15:56 +0800

    environment variable from docker push to frontend

--- src/main/java/ru/controller/StudyDController.java
+++ src/main/java/ru/controller/StudyDController.java
@@ -2,17 +2,18 @@ package ru.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudyDController {
 
     @Value("${example.environment.variable}")
-    private String val;
+    private Integer environmentVar;
 
     @GetMapping("")
-    public String showHello(){
-        System.out.println("!!!!!!!!!!" + val + "!!!!!!!!!");
+    public String showHello(Model model){
+        model.addAttribute("environmentVar", environmentVar);
         return "hello";
     }
 }
